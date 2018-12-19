5c1ae4c29a143da6e2a5666c9b44f910d8cebe70
brycegerman <r2bryce2@gmail.com>
2018-12-19 17:32:07 -0500

    Added basic slash commands
    Response to /trailermap provides an image of the trailer map, and a list of the  different selections for other maps
    
    -Problems:
    --Cannot recieve outpuit data from the  /trailermap response

--- index.js
+++ index.js
@@ -43,6 +43,9 @@
 /* Uses the slack button feature to offer a real time bot to multiple teams */
 var Botkit = require('botkit');
 
+var title = "Map: Seasonal Competition";
+var map = "default";
+
 if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
     console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
     process.exit(1);
@@ -105,16 +108,117 @@ controller.on('slash_command', function (slashCommand, message) {
 
             // If we made it here, just echo what the user typed back at them
             //TODO You do it!
-            slashCommand.replyPublic(message, "1", function() {
-                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+           slashCommand.replyPublic(message, message, function() {
+                slashCommand.replyPublicDelayed(message, "2");//.then(slashCommand.replyPublicDelayed(message, "3"));
             });
 
             break;
+
+        case "/r":
+            // if no text was supplied, treat it as a help command
+            if (message.text === "" || message.text === "Hello there") {
+                slashCommand.replyPrivate(message, "General Kenobi!");
+                return;
+            }
+        break;
+        
+        /**
+         * Gives an image of the trailer load map for competition
+         * Allows for multiple images
+         * -Dynamic changes for the future?
+         */
+        case "/trailermap":
+            //provides help for the user
+            if(message.text === "help") {
+                slashCommand.replyPrivate(message, {
+                    "text": "This command provides an image that shows an image of where each object in the trailer will be placed during load. You can select from a set of maps given in this command using the button of the map you want",
+                    "text": "You can visit the GitHub repository for more documentation on this command https://github.com"
+                });
+            } else if(message.text === "") {
+                //Im trying to make it so the command will post text with some buttons but i can't seem to get the 
+                //chat.postMessage() method to work. 
+                //The text and buttons have to be written in JSON format 
+                console.log(map);
+                slashCommand.replyPublic(message, {
+                    "text": "Trailer Load Maps",
+                    "response_type": "in_channel",
+                    "attachments": [
+                        {
+                            "title": title,
+                            "image_url": "https://pbs.twimg.com/profile_images/608621356/Team_2337_-_EngiNERDs_-_Logo_-_White_300x300_400x400.png",
+                            "fallback": "You are unable to choose a map",
+                            "callback_id": "map_chooser",
+                            "response_url": "https://hooks.slack.com/app-actions/T0MJR11A4/21974584944/yk1S9ndf35Q1flupVG5JbpM6",
+                            //map: "season",
+                            "color": "#3AA3E3",
+                            "attachment_type": "default",
+                            "actions": [
+                            {
+                                "name": "map_list",
+                                "text": "Select a trailer map...",
+                                "type": "select",
+                                "options": [
+                                    {
+                                        "text": "Season",
+                                        "value": "season",
+                                    },
+                                    {
+                                        "text": "Off Season",
+                                        "value": "offSeason",
+                                    },
+                                    {
+                                        "text": "Demos",
+                                        "value": "demos",                                       
+                                    },
+                                    { 
+                                        //for testing only
+                                        "text": "Global Thermonuclear War",
+                                        "value": "war",
+                                        "confirm": {
+                                            "title": "Are you sure?",
+                                            "text": "Are you sure you want to use this map?",
+                                            "ok_text": "Yes",
+                                            "dismiss_text": "No"
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }                  
+        );
+
+        // var obj = JSON.parse()
+
+        // map = document.getElementById("season");
+
+        /**
+         * Used to check the "map" variable and change the image based off of the value of the "map" variable
+         * @param map: String containing the value of the map that was selected. Used to change the map to the selected one
+         */
+        switch(map) {
+            case "demos":
+                slashCommand.replyPrivate(message, map);
+                break;
+            case "season":
+                slashCommand.replyPrivate(message, map);
+                break;                                                                        
+            case "offSeason":
+                slashCommand.replyPrivate(message, map);
+                break;
+                
+            default:
+                slashCommand.replyPrivate(message, "map");
+                break;
+        }
+    }
+
+        break;
         default:
             slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
 
     }
 
-})
-;
+});
 
